fa4a8f221a6cf7d42b2833629265d8ff555fe4a9
ArielChepito <arizul96@gmail.com>
2017-06-13 16:43:05 -0600

    Add y delete sobre listas, tipos de layout

--- app/src/main/java/ariel/sv/com/recyclerview_ejemplo/MainActivity.java
+++ app/src/main/java/ariel/sv/com/recyclerview_ejemplo/MainActivity.java
@@ -2,18 +2,27 @@ package ariel.sv.com.recyclerview_ejemplo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int counter = 0;
     private List<String> names;
     private RecyclerView myReclyclerView;
     private RecyclerView.Adapter myAdapter;
+    private RecyclerView.LayoutManager myLayoutManager;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,17 +34,45 @@ public class MainActivity extends AppCompatActivity {
            myAdapter = new MyAdapter(names, R.layout.recycler_view_item,new MyAdapter.onItemClickListener(){
                @Override
                public void onItemClick(String name, int position) {
-                   Toast.makeText(MainActivity.this,name + " - "+ position,Toast.LENGTH_LONG).show();
-
+                   //Toast.makeText(MainActivity.this,name + " - "+ position,Toast.LENGTH_LONG).show();
+                   deleteName(position);
 
                }
            });
-           myReclyclerView.setLayoutManager(new LinearLayoutManager(this));
+           //todos los tipos de layout manager con los que se puede jugar con el recycler view
+           myLayoutManager = new LinearLayoutManager(this);
+           myLayoutManager = new GridLayoutManager(this,2);
+           myLayoutManager = new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL);
+           //solo en caso que sepamos que el tamaño del layout no va a cambiar ahrehgamos esto e incrementa el rendimiento, aunque de nada sirve con el StraggeredGridLayout porque cambia los tamaños
+           //myReclyclerView.setHasFixedSize(true);
+           //animacion por defecto
+           myReclyclerView.setItemAnimator(new DefaultItemAnimator());
+
+           myReclyclerView.setLayoutManager(myLayoutManager);
            myReclyclerView.setAdapter(myAdapter);
        }catch(Exception e)
        {
            System.out.println("nose :" + e.getMessage());
        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.add_name:
+                this.addName(0);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+
     }
 
     private List<String> getAllNames(){
@@ -46,5 +83,16 @@ public class MainActivity extends AppCompatActivity {
             add("Ruben");
             add("Antonio");
         }};
+    }
+    private void addName(int position){
+        names.add(position, "new name "+ (++counter));
+        myAdapter.notifyItemInserted(position);
+        myLayoutManager.scrollToPosition(position);
+
+    }
+
+    private void deleteName(int position){
+        names.remove(position);
+        myAdapter.notifyItemRemoved(position);
     }
 }
